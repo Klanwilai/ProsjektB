cade531978dbc36eb933ee7aac0ffa80e5d87cf6
Daniel Nielsen <daniel@Daniels-MBP.lan>
2018-03-27 17:13:29 +0200

    First commit

---
+++ JS/scripts.js
@@ -0,0 +1,47 @@
+const sted = document.querySelector("#sted");
+const postn = document.querySelector("#post");
+
+
+function mainFunction(postnr){
+
+
+
+    postalFromCode(postnr).then (e => {
+        for(const postalCode of postalCodes){
+            if(postnr === postalCode.nr) {console.log(e, `${postnr} is ${postalCode.sted}`); sted.value = postalCode.sted;}
+        }
+    }).catch (e => {
+        postn.setAttribute("class", "invalid");
+        sted.value = " ";
+        console.log(e, `${postnr} is invalid. `);
+    });
+}
+
+function postLength(){
+    postn.setAttribute("class", "valid");
+    let post = document.querySelector("#post").value;
+    binaryIndexOf(post, postalCodes);
+    if(post.length >= 4) {mainFunction(post);}
+}
+
+/**
+*   This function returns true if param exists in postalCodes arrays
+*   @param  code    the value we want to look for in array
+*/
+function exists(code){
+    for(const postalCode of postalCodes){
+        if(code === postalCode.nr) {return true;}
+    }
+    return false;
+}
+
+/**
+*   This promise is wrapped in a function
+*   @param  code    promise is resolved if @function exists returns true
+*/
+const postalFromCode = (code) => {
+    return new Promise((resolve, reject) => {
+        if(exists(code)) {resolve("Everything is OK");}
+        else {reject(Error("Nothing is OK"));}
+    });
+}
